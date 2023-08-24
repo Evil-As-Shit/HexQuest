33e2eb31b8009853b9ff14ff03088129228b873e
Anthony <messinabrothers@gmail.com>
2023-08-23 20:06:38 -0700

    feat: Dialogue slowly prints one character at a time

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -3,3 +3,7 @@
 extends Node
 
 var dict_dialogue = {}
+
+var time_dialogue: float = .025
+
+var flag_displaying_dialogue: bool = false
--- Scripts/UI/DialogueBox.gd
+++ Scripts/UI/DialogueBox.gd
@@ -1,19 +1,32 @@
 extends Control
 
+var timer_display: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
 	
 	SignalController.display_dialogue.connect(on_display_dialogue)
+	
+func _process(delta):
+	if (timer_display > 0):
+		timer_display -= delta
+		if (timer_display <= 0):
+			$Label.visible_characters += 1
+			if ($Label.visible_ratio >= 1):
+				GameData.flag_displaying_dialogue = false
+			else:
+				while (timer_display < 0): timer_display += GameData.time_dialogue
 
 func on_display_dialogue(id: String):
 	visible = true;
+	GameData.flag_displaying_dialogue = true
 	
 	var text = GameData.dict_dialogue[id]["text"]
 	var expression = GameData.dict_dialogue[id]["expression"]
 	var t = null if (expression == "null") else load("res://Assets/ui/Avatars/" + expression + ".png")
 	
 	$Label.text = text
+	$Label.visible_characters = 0
 	$PortraitTexture.texture = t
 	
+	timer_display = GameData.time_dialogue
