b18c627fb4f6a3a84ae640dd996bb60d56a37469
Anthony <messinabrothers@gmail.com>
2023-09-10 18:24:30 -0700

    feat: NPC object (looks like a bookcase) can be interacted with.

---
+++ Scenes/Objects/NPC.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+var dialogue_id: String = "NPC000"
+
+@onready var object_id : int = get_node("Object_Id").get_instance_id()
+
+func _ready():
+	SignalController.interaction_detected.connect(object_interacted)
+
+func object_interacted(object: int, _player: CharacterBody2D):
+	if(object == object_id):
+		SignalController.emit_signal("display_dialogue", dialogue_id)
--- Scripts/InputController.gd
+++ Scripts/InputController.gd
@@ -3,12 +3,31 @@ extends Node
 
 func _input(event):
 	if event is InputEventKey and event.pressed and not event.is_echo():
-		#Pressing E While Not On Phone (object interaction)
-		if event.keycode == KEY_E and !GameData.is_using_phone:
-			SignalController.emit_signal("interaction_check")
-		#Pressing E While On Phone (phone interaction)
-		if event.keycode == KEY_E and GameData.is_using_phone:
-			SignalController.emit_signal("phone_interacted")
-		#Pressing Q toggles phone open and close
-		if event.keycode == KEY_Q:
-			SignalController.emit_signal("toggle_phone")
+		if GameData.is_using_phone:
+			match event.keycode:
+				KEY_E:
+					SignalController.emit_signal("phone_interacted")
+				KEY_Q:
+					SignalController.emit_signal("toggle_phone")
+		elif GameData.flag_displaying_dialogue:
+			SignalController.emit_signal("finish_dialogue")
+		elif GameData.flag_waiting_dialogue_prompt:
+			print("choice made! ", GameData.dialogue_choice_id)
+			SignalController.emit_signal("close_dialogue")
+		elif GameData.flag_waiting_dialogue_next:
+			SignalController.emit_signal("close_dialogue")
+		else:
+			match event.keycode:
+				KEY_E:
+					SignalController.emit_signal("interaction_check")
+				KEY_Q:
+					SignalController.emit_signal("toggle_phone")
+#		#Pressing E While Not On Phone (object interaction)
+#		if event.keycode == KEY_E and !GameData.is_using_phone:
+#			SignalController.emit_signal("interaction_check")
+#		#Pressing E While On Phone (phone interaction)
+#		if event.keycode == KEY_E and GameData.is_using_phone:
+#			SignalController.emit_signal("phone_interacted")
+#		#Pressing Q toggles phone open and close
+#		if event.keycode == KEY_Q:
+#			SignalController.emit_signal("toggle_phone")
